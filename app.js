f9d09a5f2e7888f135bc2e5f53dff12aab6a7505
IVKrylova <ivk.lis@yandex.ru>
2022-07-25 16:25:11 +0300

    feat: install body-parser

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000, MONGODB_URL, NODE_ENV } = process.env;
 const MONGODB_URL_DEV = require('./utils/constants');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 async function main() {
   await mongoose.connect(NODE_ENV === 'production' ? MONGODB_URL : MONGODB_URL_DEV);
