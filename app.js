04ce170469b74ccf0ca505c6f90a322f2b24efab
IVKrylova <ivk.lis@yandex.ru>
2022-07-25 16:26:52 +0300

    feat: install helmet

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const { PORT = 3000, MONGODB_URL, NODE_ENV } = process.env;
 const MONGODB_URL_DEV = require('./utils/constants');
 
 const app = express();
+
+// защита приложения от веб-уязвимостей путем соответствующей настройки заголовков HTTP
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
