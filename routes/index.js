36f0fd140da6f3be7180174489f92e4469c0ad97
IVKrylova <ivk.lis@yandex.ru>
2022-07-25 16:59:15 +0300

    fix: literal error

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { NotFoundError } = require('../utils/NotFoundError');
-const { WRONG_PATH_MESSAGE } = require('../utils/constants');
+const { WRONG_PATCH_MESSAGE } = require('../utils/constants');
 
 // oбработка неправильного пути
 router.use((req, res, next) => {
-  next(new NotFoundError(WRONG_PATH_MESSAGE));
+  next(new NotFoundError(WRONG_PATCH_MESSAGE));
 });
 
 module.exports = router;
--- utils/constants.js
+++ utils/constants.js
@@ -2,11 +2,11 @@
 const MONGODB_URL_DEV = 'mongodb://localhost:27017/krylovdb';
 
 // сообщения ошибок
-const WRONG_PATH_MESSAGE = 'Страница не найдена';
+const WRONG_PATCH_MESSAGE = 'Страница не найдена';
 const INTERNAL_ERROR_MESSAGE = 'Внутренняя ошибка сервера';
 
 module.exports = {
   MONGODB_URL_DEV,
-  WRONG_PATH_MESSAGE,
+  WRONG_PATCH_MESSAGE,
   INTERNAL_ERROR_MESSAGE,
 };
