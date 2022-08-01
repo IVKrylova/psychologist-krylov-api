9f0f07f11b2a5af9ec170f05e7c96b9d5870c6eb
IVKrylova <ivk.lis@yandex.ru>
2022-08-01 13:50:07 +0300

    feat: add router for getting apointments

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const { celebrate } = require('celebrate');
-const { signup } = require('../controllers/calendar');
+const { signup, getAppointments } = require('../controllers/calendar');
 const { signUpValidation } = require('../utils/celebrateValidation');
 
 // добавление записи через календарь
 router.post('/calendar', celebrate(signUpValidation), signup);
+// получение данных из календаря
+router.get('/calendar', getAppointments);
 
 module.exports = router;
